c1c6d3e32c2f5529d3f77ca77a59fd96156789ca
Kane-TM <0306181335@caothang.edu.vn>
2020-11-30 22:43:07 +0700

    user activity screen has done

--- app/src/main/java/com/example/tech4life/MainActivity.java
+++ app/src/main/java/com/example/tech4life/MainActivity.java
@@ -8,10 +8,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 //        setContentView(R.layout.report_screen);
 //        setContentView(R.layout.login_activity_screen);
         //setContentView(R.layout.register_activity_screen);
      //   setContentView(R.layout.post_activity_screen);
+
+        setContentView(R.layout.user_activity_screen);
     }
 }
