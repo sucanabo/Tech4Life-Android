c62728eff155cca597b78547dfe51d48140e7c45
Phuc <sucanabo@gmail.com>
2020-12-23 14:24:59 +0700

    ex/224_2312

--- app/src/main/java/com/example/tech4life/MainActivity.java
+++ app/src/main/java/com/example/tech4life/MainActivity.java
@@ -11,6 +11,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     
     DrawerLayout drawerLayout;
@@ -28,10 +31,7 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.post_activity_screen);
 
 //        setContentView(R.layout.user_activity_screen);
-
         //setContentView(R.layout.user_activity_screen);
-
-
         //Assign variable
         drawerLayout = findViewById(R.id.drawer_layout);
         //recycler post item
