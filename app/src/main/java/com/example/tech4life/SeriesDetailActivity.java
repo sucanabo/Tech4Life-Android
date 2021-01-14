f7781143f7a1155354a9a21a51f202c7d54d2eb0
Phuc <sucanabo@gmail.com>
2021-01-14 20:42:25 +0700

    dialog_report

--- app/src/main/java/com/example/tech4life/SeriesDetailActivity.java
+++ app/src/main/java/com/example/tech4life/SeriesDetailActivity.java
@@ -3,6 +3,12 @@ package com.example.tech4life;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class SeriesDetailActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class SeriesDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_series_detail);
+    }
+    public void clickBackToPrevious(View view) {
+
+        onBackPressed();
+    }
+    public void ClickMore(View view){
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(SeriesDetailActivity.this,R.style.BottomSheetDialogTheme);
+        final View bottomSheetView = LayoutInflater.from(getApplicationContext())
+                .inflate(R.layout.bottom_sheet_series,(LinearLayout)findViewById(R.id.bottom_sheet_container));
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
+    }
+    public void ClickReport(View view){
+        Toast.makeText(this,"report",Toast.LENGTH_SHORT).show();
     }
 }
